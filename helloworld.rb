e722f1ba9a7c63b2a8020f7a8a417ed73102ee9f
Salma <71652237+Sallyberry@users.noreply.github.com>
2020-11-18 14:01:52 +0100

    Update helloworld.rb
    ajout each method

--- helloworld.rb
+++ helloworld.rb
@@ -2,11 +2,11 @@ require 'json'
 require 'open-uri'
 file =  JSON.load(URI.open('https://raw.githubusercontent.com/adatechschool/Projets/master/robot_de_conversation/recipes.json'))
 # data_hash = JSON.parse(file)
-gets "Gateau au chocolat"
-puts file[1]["recipe_name"]
+#gets "Gateau au chocolat"
+# puts file[1]["recipe_name"]
 
+file.each do |recipe| 
+    puts recipe ["recipe_name"] 
+    puts recipe ["ingredients"] ["Sucre"]
+end
 
-# print"hello world"
-
-# inp = $stdin.read
-# puts inpcd cd 
