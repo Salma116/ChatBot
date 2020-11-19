af5cf38478d84c1dbd2d1f0bfe001c324e85ab35
josiane Béké <josybekette@gmail.com>
2020-11-19 18:12:25 +0100

    recuperation des elements du tableau

--- helloworld.rb
+++ helloworld.rb
@@ -4,9 +4,20 @@ file =  JSON.load(URI.open('https://raw.githubusercontent.com/adatechschool/Proj
 # data_hash = JSON.parse(file)
 #gets "Gateau au chocolat"
 # puts file[1]["recipe_name"]
+puts"Salut, comment vas-tu? C'est quoi ton petit nom?"
+name=gets.chomp.capitalize
+puts"Salut à toi #{name}. En quoi puis-je t'aider?"
+question =gets.chomp.capitalize
+puts"Avec du #{question}, je te propose des recettes à base de #{question}"
+result=[]
 
 file.each do |recipe| 
-    puts recipe ["recipe_name"] 
-    puts recipe ["ingredients"] ["Sucre"]
-end
+    
+    if recipe["ingredients"].include? question 
+        result.push(recipe)   
+    end
+end  
 
+result.each do|recipe|
+    puts recipe["recipe_name"]
+end  
