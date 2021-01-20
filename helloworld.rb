7bf49ff5221490d6f6899b2a0c89119a7a4ae9a2
salma116 <>
2021-01-20 12:22:21 +0100

    console affiche toute la recette
    On peut écrire dans la console un seul ingrédients précis (non sensible à la casse). On peut écrire le nom d'une recette.
    La console affichera alors les recettes ayant l'ingrédient ou correspondant au nom.

--- helloworld.rb
+++ helloworld.rb
@@ -11,13 +11,28 @@ question =gets.chomp.capitalize
 puts"Avec du #{question}, je te propose des recettes à base de #{question}"
 result=[]
 
-file.each do |recipe| 
-    
-    if recipe["ingredients"].include? question 
-        result.push(recipe)   
+file.each do |recipe|
+    if recipe["ingredients"].include? question
+        result.push(recipe)
     end
-end  
+end
+file.each do |recipename|   #Pour trouver à partir de nom de recette.
+    if recipename["recipe_name"].include? question
+        result.push(recipename)
+    end
+end
+
+ingredients=[] #création array pour y stocker les ingrédients
+
+result[0]["ingredients"].each do |mesingredients| #mesingredients est la variable locale
+ingredients.push(mesingredients) #j'ajoute les éléments de l'array ingredients dans la variable locale mesingredients
+end
 
 result.each do|recipe|
     puts recipe["recipe_name"]
-end  
+    puts ingredients
+    puts recipe["steps"]
+end
+
+result.each do|recipename|
+end
